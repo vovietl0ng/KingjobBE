dfc6a21375df2e8d3bdd5207eaf318d3a45c3188
long <vovietlong123@gmail.com>
2023-11-22 13:41:10 +0700

    Iuser service

---
+++ Application/Catalog/IUserService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using ViewModel.Catalog.User;
+using ViewModel.Common;
+
+namespace Application.Catalog
+{
+    public interface IUserService
+    {
+
+        Task<ApiResult<UserInformationViewModel>> GetUserInformation(Guid userId);
+        Task<ApiResult<UserAvatarViewModel>> GetUserAvatar(Guid userId);
+        Task<ApiResult<List<AllUserResult>>> GetAllUser();
+        Task<ApiResult<bool>> UpdateUserInformation(UserUpdateRequest request);
+        Task<ApiResult<bool>> UpdateUserAvatar(int id, IFormFile thumnailImage);
+        Task<ApiResult<bool>> FollowCompany(Guid userId, Guid companyId);
+        Task<ApiResult<bool>> ChangePasswordUser(ChangePasswordUserRequest request);
+        Task<ApiResult<bool>> ForgotPassword(ForgotPasswordRequest request);
+        Task<ApiResult<bool>> SubmitCV(SubmitCVRequest request);
+
+
+    }
+}
